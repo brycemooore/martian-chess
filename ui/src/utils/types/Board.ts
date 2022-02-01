c29310dfcc38b0fb6e4efcf278778c5fb96e2907
brycemooore <brycemooore@gmail.com>
2022-02-01 14:40:34 -0500

    some more board

--- ui/src/utils/types/Board.ts
+++ ui/src/utils/types/Board.ts
@@ -1,6 +1,20 @@
-import { Tile } from "./Tile";
+import { Piece, Tile } from ".";
 
 export interface BoardState {
     board: Array<Array<Tile>>
-    
+    playerAPieces: Array<Piece>
+    playerBPieces: Array<Piece>
+}
+
+export class Board implements BoardState {
+    board: Tile[][];
+    playerAPieces: Piece[];
+    playerBPieces: Piece[];
+
+    constructor(fen: string) {
+        this.board = []
+        this.playerAPieces = []
+        this.playerBPieces = []
+    }
+    static BoardState 
 }
