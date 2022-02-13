2a9cb70c5ce1eb2f7f385aa9b46ff788bf692afd
jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air.local>
2022-02-13 14:04:20 -0500

    add tests, center board, fix rotate bug

--- ui/src/components/board.tsx
+++ ui/src/components/board.tsx
@@ -1,69 +1,68 @@
 import Tile from "./tile";
 
 interface BoardProps {
-  rotation?: string
+	rotation?: string;
 }
 
-export default function Board({ rotation = '' }: BoardProps) {
-  return (
-    <div className={`flex ${rotation}`}>
-      <div className="flex flex-col">
-        <div className="flex flex-col mb-10">
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-        </div>
-        <div className="flex-col">
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <div className="flex flex-col mb-10">
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-        </div>
-        <div className="flex-col">
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <div className="flex flex-col mb-10">
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-        </div>
-        <div className="flex-col">
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <div className="flex flex-col mb-10">
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-        </div>
-        <div className="flex-col">
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-          <Tile color="bg-board-red" />
-          <Tile color="bg-board-blue" />
-        </div>
-      </div>
-
-    </div>
-  );
+export default function Board({ rotation = "" }: BoardProps) {
+	return (
+		<div data-testid="board" className={`flex ${rotation}`}>
+			<div className="flex flex-col">
+				<div className="flex flex-col mb-10">
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+				</div>
+				<div className="flex-col">
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+				</div>
+			</div>
+			<div className="flex flex-col">
+				<div className="flex flex-col mb-10">
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+				</div>
+				<div className="flex-col">
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+				</div>
+			</div>
+			<div className="flex flex-col">
+				<div className="flex flex-col mb-10">
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+				</div>
+				<div className="flex-col">
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+				</div>
+			</div>
+			<div className="flex flex-col">
+				<div className="flex flex-col mb-10">
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+				</div>
+				<div className="flex-col">
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+					<Tile color="bg-board-red" />
+					<Tile color="bg-board-blue" />
+				</div>
+			</div>
+		</div>
+	);
 }
--- ui/src/components/tile.tsx
+++ ui/src/components/tile.tsx
@@ -1,9 +1,9 @@
 interface TileProps {
-  color: string
+	color: string;
 }
 
 export default function Tile({ color }: TileProps) {
-  return (
-    <div className={`w-16 h-16 md:w-24 md:h-24 sm:w-16 sm:h-16 ${color}`} />
-  );
+	return (
+		<div className={`w-16 h-16 md:w-24 md:h-24 sm:w-16 sm:h-16 ${color}`} />
+	);
 }
