18f9e390c744807d07b780ec6a5edf8c6b47ba3a
brycemooore <brycemooore@gmail.com>
2022-02-13 15:55:59 -0500

    add file

--- ui/src/components/tile.tsx
+++ ui/src/components/tile.tsx
@@ -1,12 +1,12 @@
 interface TileProps {
-	color: string;
-	piece?: string;
+  color: string;
+  piece?: string;
 }
 
 export default function Tile({ color, piece }: TileProps) {
-	return (
-		<div className={`w-16 h-16 md:w-24 md:h-24 sm:w-16 sm:h-16 ${color}`}>
-			{piece}
-		</div>
-	);
+  return (
+    <div className={`w-16 h-16 md:w-24 md:h-24 sm:w-16 sm:h-16 ${color}`}>
+      {piece}
+    </div>
+  );
 }
