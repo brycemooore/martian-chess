fd0237ae053c079e1416aff2c05c8afabf5d8826
Jacob Short <46907893+jrshort89@users.noreply.github.com>
2022-02-13 15:40:36 -0500

    rudimentary piece mapper that is definitely simple and scaleable (#8)
    Co-authored-by: jrshort89@gmail.com <jakeshort@Jakes-MacBook-Air.local>

--- ui/src/components/board.tsx
+++ ui/src/components/board.tsx
@@ -5,62 +5,65 @@ interface BoardProps {
 }
 
 export default function Board({ rotation = "" }: BoardProps) {
+	const pieceMap = "QQD1/QDP1/DPP1/4/4/1PPD/1PDQ/1DQQ"
+		.split("/")
+		.map((p) => p.split(""));
 	return (
-		<div data-testid="board" className={`flex ${rotation}`}>
+		<div className={`flex ${rotation}`}>
 			<div className="flex flex-col">
 				<div className="flex flex-col mb-10">
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
+					<Tile piece={pieceMap[0][0]} color="bg-board-blue" />
+					<Tile piece={pieceMap[1][0]} color="bg-board-red" />
+					<Tile piece={pieceMap[2][0]} color="bg-board-blue" />
+					<Tile piece={pieceMap[3][0]} color="bg-board-red" />
 				</div>
 				<div className="flex-col">
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
+					<Tile piece={pieceMap[4][0]} color="bg-board-blue" />
+					<Tile piece={pieceMap[5][0]} color="bg-board-red" />
+					<Tile piece={pieceMap[6][0]} color="bg-board-blue" />
+					<Tile piece={pieceMap[7][0]} color="bg-board-red" />
 				</div>
 			</div>
 			<div className="flex flex-col">
 				<div className="flex flex-col mb-10">
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
+					<Tile piece={pieceMap[0][1]} color="bg-board-red" />
+					<Tile piece={pieceMap[1][1]} color="bg-board-blue" />
+					<Tile piece={pieceMap[2][1]} color="bg-board-red" />
+					<Tile piece={pieceMap[3][1]} color="bg-board-blue" />
 				</div>
 				<div className="flex-col">
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
+					<Tile piece={pieceMap[4][1]} color="bg-board-red" />
+					<Tile piece={pieceMap[5][1]} color="bg-board-blue" />
+					<Tile piece={pieceMap[6][1]} color="bg-board-red" />
+					<Tile piece={pieceMap[7][1]} color="bg-board-blue" />
 				</div>
 			</div>
 			<div className="flex flex-col">
 				<div className="flex flex-col mb-10">
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
+					<Tile piece={pieceMap[0][2]} color="bg-board-blue" />
+					<Tile piece={pieceMap[1][2]} color="bg-board-red" />
+					<Tile piece={pieceMap[2][2]} color="bg-board-blue" />
+					<Tile piece={pieceMap[3][2]} color="bg-board-red" />
 				</div>
 				<div className="flex-col">
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
+					<Tile piece={pieceMap[4][2]} color="bg-board-blue" />
+					<Tile piece={pieceMap[5][2]} color="bg-board-red" />
+					<Tile piece={pieceMap[6][2]} color="bg-board-blue" />
+					<Tile piece={pieceMap[7][2]} color="bg-board-red" />
 				</div>
 			</div>
 			<div className="flex flex-col">
 				<div className="flex flex-col mb-10">
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
+					<Tile piece={pieceMap[0][3]} color="bg-board-red" />
+					<Tile piece={pieceMap[1][3]} color="bg-board-blue" />
+					<Tile piece={pieceMap[2][3]} color="bg-board-red" />
+					<Tile piece={pieceMap[3][3]} color="bg-board-blue" />
 				</div>
 				<div className="flex-col">
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
-					<Tile color="bg-board-red" />
-					<Tile color="bg-board-blue" />
+					<Tile piece={pieceMap[4][3]} color="bg-board-red" />
+					<Tile piece={pieceMap[5][3]} color="bg-board-blue" />
+					<Tile piece={pieceMap[6][3]} color="bg-board-red" />
+					<Tile piece={pieceMap[7][3]} color="bg-board-blue" />
 				</div>
 			</div>
 		</div>
--- ui/src/components/tile.tsx
+++ ui/src/components/tile.tsx
@@ -1,9 +1,12 @@
 interface TileProps {
 	color: string;
+	piece?: string;
 }
 
-export default function Tile({ color }: TileProps) {
+export default function Tile({ color, piece }: TileProps) {
 	return (
-		<div className={`w-16 h-16 md:w-24 md:h-24 sm:w-16 sm:h-16 ${color}`} />
+		<div className={`w-16 h-16 md:w-24 md:h-24 sm:w-16 sm:h-16 ${color}`}>
+			{piece}
+		</div>
 	);
 }
